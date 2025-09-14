7c35f51937c7d027aa8d18f7ddbc49af12864449
yoampies <andresampies@gmail.com>
2025-09-14 11:55:45 -0400

    added images

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -3,13 +3,17 @@ import { FaRegChartBar, FaGithub, FaReact, FaMobileAlt } from 'react-icons/fa';
 import { RiCodeFill, RiJavascriptLine, RiPencilLine, RiTailwindCssFill } from "react-icons/ri";
 import { GiGearStickPattern } from "react-icons/gi";
 import { HiOutlineDeviceMobile } from 'react-icons/hi';
+import parasaite from "../assets/demos/parasaite.png"
+import studyai from "../assets/demos/studyai.png"
+import gabo_astral from "../assets/demos/gabo-astral.png"
+import jose from "../assets/demos/jose.png"
 
 export const projects = [
   {
     id: 1,
     title: "AI-Powered Parasite Detection Platform",
     description: "A frontend application that simulates parasite detection from images using Web Workers and 3D visualization.",
-    imgURL: "http://your-domain.com/images/parasitos-app.jpg",
+    imgURL: parasaite,
     longDescription: "I developed a frontend application that simulates parasite detection from user-uploaded images. I implemented Web Workers to simulate image processing without blocking the UI, and used the Canvas API to draw randomly generated 'detection' boxes. The history of uploaded images is saved locally using localStorage. For epidemiological data visualization, I integrated D3.js to create interactive charts. Additionally, I integrated React Three Fiber to render 3D models of parasites, demonstrating skills in data visualization and 3D graphics.",
     demoURL: "https://parasaite.vercel.app",
     githubURL: "https://github.com/yoampies/parasaite",
@@ -30,7 +34,7 @@ export const projects = [
     id: 2,
     title: "AI Study Tool for Students",
     description: "An application that simulates generating summaries, quizzes, and flashcards from text, PDFs, and audio.",
-    imgURL: "http://your-domain.com/images/estudios-ia-app.jpg",
+    imgURL: studyai,
     longDescription: "I created a frontend application for students that simulates generating study content from various sources like text, PDFs, and audio. The app simulates interaction with AI to create summaries, multiple-choice quizzes, and flashcards. I used localStorage to save the creation history. GSAP was used to animate the 'AI processing' progress and flashcard transitions, providing a fluid and engaging user experience. Built with React and Tailwind CSS.",
     demoURL: "https://studyai-drab.vercel.app",
     githubURL: "https://github.com/yoampies/studyai",
@@ -47,7 +51,7 @@ export const projects = [
     id: 3,
     title: "Interactive Astrology App",
     description: "A React app with 3D animations and visuals for astrology and tarot services.",
-    imgURL: "http://your-domain.com/images/astrologia-app.jpg",
+    imgURL: gabo_astral,
     longDescription: "I developed an interactive application for astrology and tarot services. The app was built with React and Tailwind CSS, and integrates React Three Fiber for an immersive 3D experience. It includes a main animation where users can select tarot cards to ask questions, with detailed explanations of their meanings. The interface is designed to present the astrologer's and tarot reader's services in a clear and appealing way.",
     demoURL: "https://gabo-astral.vercel.app",
     githubURL: "https://github.com/yoampies/gabo_astral",
@@ -62,7 +66,7 @@ export const projects = [
     id: 4,
     title: "Psychology Website with Scrollytelling",
     description: "An informative website for a psychologist, with a responsive design and scrollytelling animations.",
-    imgURL: "http://your-domain.com/images/psicologia-app.jpg",
+    imgURL: jose,
     longDescription: "I designed and developed a website for a psychologist using React and Tailwind CSS. The main feature is the use of GSAP to implement 'scrollytelling' animations, guiding the user through information about the professional's services in an interactive and visually attractive way. The design is fully responsive, ensuring an optimal experience on all devices.",
     demoURL: "https://jose-nine-ashy.vercel.app",
     githubURL: "https://github.com/yoampies/jose",
